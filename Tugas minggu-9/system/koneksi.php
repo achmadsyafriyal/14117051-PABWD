3a4a479794833f03722aa1e5ab2cfaa13fa5d64d
achmadsyafriyal <50198991+achmadsyafriyal@users.noreply.github.com>
2019-11-11 21:41:07 +0700

    tugas

---
+++ Tugas minggu-9/system/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost'; // Nama hostnya
+$username = 'root'; // Username
+$password = ''; // Password (Isi jika menggunakan password)
+$database = 'modul_9'; // Nama databasenya
+$base_url = 'http://localhost/14117103-PABWD/Tugas/'; // Set Base Url Web
+
+// Koneksi ke MySQL dengan MySQLi
+$connect = mysqli_connect($host, $username, $password, $database);
+?>
